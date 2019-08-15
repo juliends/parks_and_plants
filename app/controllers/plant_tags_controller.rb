38edef7ff8627f3e490579af92201c454a9e0da6
julien da silva <julien.da.silva@hotmail.com>
2019-08-15 18:35:05 +0200

    livecode done

---
+++ app/controllers/plant_tags_controller.rb
@@ -0,0 +1,21 @@
+class PlantTagsController < ApplicationController
+  def new
+    @plant = Plant.find(params[:plant_id])
+    @plant_tag = PlantTag.new
+  end
+
+  def create
+    @plant = Plant.find(params[:plant_id])
+    @tags = Tag.where(id: params[:plant_tag][:tag_id])
+    @tags.each do |tag|
+      PlantTag.create(tag: tag, plant: @plant)
+    end
+    redirect_to garden_path(@plant.garden)
+  end
+
+  private
+
+  def plant_tag_params
+    params.require(:plant_tag).permit(:tag_id)
+  end
+end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,4 @@
+import 'select2/dist/css/select2.css';
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
